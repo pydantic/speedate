b727783c866cb2a39b90088de6f4bf500b592fc7
Samuel Colvin <s@muelcolvin.com>
2022-05-30 14:05:14 +0100

    add combined bench

--- benches/main.rs
+++ benches/main.rs
@@ -96,3 +96,37 @@ fn time(bench: &mut Bencher) {
         black_box(Time::parse_str(&s).unwrap());
     })
 }
+
+#[bench]
+fn x_combined(bench: &mut Bencher) {
+    let dt1 = black_box("1997-09-09T09:09:09Z");
+    let dt2 = black_box("1997-09-09 09:09:09");
+    let dt3 = black_box("2000-02-29 01:01:50.123456");
+    let dt4 = black_box("2000-02-29 01:01:50.123456+08:00");
+
+    let d1 = black_box("1997-09-09");
+    let d2 = black_box("2000-02-29");
+    let d3 = black_box("2001-02-28");
+    let d4 = black_box("2001-12-28");
+
+    let t1 = black_box("12:13");
+    let t2 = black_box("12:13:14");
+    let t3 = black_box("12:13:14.123");
+    let t4 = black_box("12:13:14.123456");
+    bench.iter(|| {
+        black_box(DateTime::parse_str(&dt1).unwrap());
+        black_box(DateTime::parse_str(&dt2).unwrap());
+        black_box(DateTime::parse_str(&dt3).unwrap());
+        black_box(DateTime::parse_str(&dt4).unwrap());
+
+        black_box(Date::parse_str(&d1).unwrap());
+        black_box(Date::parse_str(&d2).unwrap());
+        black_box(Date::parse_str(&d3).unwrap());
+        black_box(Date::parse_str(&d4).unwrap());
+
+        black_box(Time::parse_str(&t1).unwrap());
+        black_box(Time::parse_str(&t2).unwrap());
+        black_box(Time::parse_str(&t3).unwrap());
+        black_box(Time::parse_str(&t4).unwrap());
+    })
+}
