6c402fff81773a93a818bd8ed421d5af6150f1c8
Samuel Colvin <s@muelcolvin.com>
2022-05-31 12:33:04 +0100

    adding coverage to CI (#3)
    * adding coverage to CI
    
    * fix coverage script
    
    * test
    
    * fix ci, add codecov.yml
    
    * try always using rustup but caching
    
    * test commit
    
    * add coverage badge to readme

---
+++ tests/test_coverage_export.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -d "target/debug/deps" ]; then
+  find target/debug/deps -regex '.*/main[^.]*' -delete
+fi
+
+RUSTFLAGS='-C instrument-coverage' cargo test
+
+rust-profdata merge -sparse default.profraw -o default.profdata
+
+rust-cov report -Xdemangler=rustfilt $(find target/debug/deps -regex '.*/main[^.]*') \
+    -instr-profile=default.profdata \
+    --ignore-filename-regex='/.cargo/registry' \
+    --ignore-filename-regex='library/std' \
+    --ignore-filename-regex='tests/main.rs'
+
+rust-cov export -Xdemangler=rustfilt $(find target/debug/deps -regex '.*/main[^.]*') \
+    -instr-profile=default.profdata \
+    --ignore-filename-regex='/.cargo/registry' \
+    --ignore-filename-regex='library/std' \
+    --ignore-filename-regex='tests/main.rs' \
+    -format=lcov > default.lcov
+
+rm default.profraw default.profdata
--- tests/test_coverage_html.sh
+++ tests/test_coverage_html.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-# to avoid including this in `ls ...` subcommand below
-rm target/debug/deps/not8601*.d
-rm -rf htmlcov
+set -e
+
+if [ -d "target/debug/deps" ]; then
+  find target/debug/deps -regex '.*/main[^.]*' -delete
+fi
+
+RUSTFLAGS='-C instrument-coverage' cargo test
 
 rust-profdata merge -sparse default.profraw -o default.profdata
 
@@ -10,11 +14,15 @@ rust-cov report -Xdemangler=rustfilt $(find target/debug/deps -regex '.*/main[^.
     -instr-profile=default.profdata \
     --ignore-filename-regex='/.cargo/registry' \
     --ignore-filename-regex='library/std' \
+    --ignore-filename-regex='tests/main.rs'
+
+rm -rf htmlcov
 
 rust-cov show -Xdemangler=rustfilt $(find target/debug/deps -regex '.*/main[^.]*') \
     -instr-profile=default.profdata \
     --ignore-filename-regex='/.cargo/registry' \
     --ignore-filename-regex='library/std' \
+    --ignore-filename-regex='tests/main.rs' \
     -format=html -o htmlcov
 
 rm default.profraw default.profdata
