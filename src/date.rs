5654ca55e83d3a69ae58b42f8bff04926c40a3f3
mkdir700 <mkdir700@gmail.com>
2023-01-14 18:21:46 +0800

    override the check for `clippy::bool_to_int_with_if`

--- src/date.rs
+++ src/date.rs
@@ -1,4 +1,3 @@
-#![allow(clippy::bool_to_int_with_if)]
 use std::fmt;
 
 use crate::{get_digit_unchecked, DateTime, ParseError};
@@ -178,6 +177,7 @@ impl Date {
     }
 
     /// Day of the year, starting from 1.
+    #[allow(clippy::bool_to_int_with_if)]
     pub fn ordinal_day(&self) -> u16 {
         let leap_extra = if is_leap_year(self.year) { 1 } else { 0 };
         let day = self.day as u16;
