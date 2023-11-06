6e686099222c5b94787b41c644af3672026fdedd
David Hewitt <david@hewitt.tech>
2023-11-06 12:40:10 +0000

    no longer accept '-' or '+' as valid numeric strings (#52)
    * no longer accept '-' or '+' as valid numeric strings
    
    * unwrap error
    
    Co-authored-by: Samuel Colvin <s@muelcolvin.com>
    
    * Update tests/main.rs
    
    ---------
    
    Co-authored-by: Samuel Colvin <s@muelcolvin.com>

--- src/numbers.rs
+++ src/numbers.rs
@@ -2,35 +2,28 @@
 ///
 /// This is around 2x faster than using `str::parse::<i64>()`
 pub fn int_parse_str(s: &str) -> Option<i64> {
-    fast_parse_int(s.bytes())
+    int_parse_bytes(s.as_bytes())
 }
 
 /// Parse bytes as an int.
 pub fn int_parse_bytes(s: &[u8]) -> Option<i64> {
-    fast_parse_int(s.iter().copied())
-}
-
-pub fn fast_parse_int<I: Iterator>(mut bytes: I) -> Option<i64>
-where
-    I: Iterator<Item = u8>,
-{
-    let mut result: i64 = 0;
-    let neg = match bytes.next() {
-        Some(b'-') => true,
-        Some(b'+') => false,
-        Some(c) if c.is_ascii_digit() => {
-            result = (c & 0x0f) as i64;
-            false
-        }
+    let (neg, first_digit, digits) = match s {
+        [b'-', first, digits @ ..] => (true, first, digits),
+        [b'+', first, digits @ ..] | [first, digits @ ..] => (false, first, digits),
         _ => return None,
     };
 
-    for digit in bytes {
+    let mut result = match first_digit {
+        b'0' => 0,
+        b'1'..=b'9' => (first_digit & 0x0f) as i64,
+        _ => return None,
+    };
+
+    for digit in digits {
+        result = result.checked_mul(10)?;
         match digit {
-            b'0'..=b'9' => {
-                result = result.checked_mul(10)?;
-                result = result.checked_add((digit & 0x0f) as i64)?
-            }
+            b'0' => {}
+            b'1'..=b'9' => result = result.checked_add((digit & 0x0f) as i64)?,
             _ => return None,
         }
     }
@@ -58,29 +51,26 @@ impl IntFloat {
 ///
 /// This is around 2x faster than using `str::parse::<f64>()`
 pub fn float_parse_str(s: &str) -> IntFloat {
-    fast_parse_float(s.bytes())
+    float_parse_bytes(s.as_bytes())
 }
 
 /// Parse bytes as an float.
 pub fn float_parse_bytes(s: &[u8]) -> IntFloat {
-    fast_parse_float(s.iter().copied())
-}
+    let (neg, first_digit, digits) = match s {
+        [b'-', first, digits @ ..] => (true, first, digits),
+        [b'+', first, digits @ ..] | [first, digits @ ..] => (false, first, digits),
+        _ => return IntFloat::Err,
+    };
 
-pub fn fast_parse_float<I: Iterator>(mut bytes: I) -> IntFloat
-where
-    I: Iterator<Item = u8>,
-{
-    let mut int_part: i64 = 0;
-    let neg = match bytes.next() {
-        Some(b'-') => true,
-        Some(c) if c.is_ascii_digit() => {
-            int_part = (c & 0x0f) as i64;
-            false
-        }
+    let mut int_part = match first_digit {
+        b'0' => 0,
+        b'1'..=b'9' => (first_digit & 0x0f) as i64,
         _ => return IntFloat::Err,
     };
 
     let mut found_dot = false;
+
+    let mut bytes = digits.iter().copied();
 
     for digit in bytes.by_ref() {
         match digit {
